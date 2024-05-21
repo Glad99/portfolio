f7d8ab45d58aef9a7f30b662b5510414941c87fe
Gladys <gladysalu1@gmail.com>
2024-05-21 22:43:08 +0100

    commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,11 +5,11 @@ const Project = () => {
     <div id='projects' className=' bg-blue pt-5 lg:pt-12'>
       <h2 className='text-2xl font-bold md:text-3xl mb-10 text-gray-500 text-center'>Projects</h2>
       <div className='flex lg:flex-col gap-20 mx-[2rem] lg:mx-[15rem]'>
-        <div className='flex gap-20'><div className=' h-[20rem] w-[30rem] bg-slate-600'>.</div>
-      <div className=' bg-slate-500 h-[20rem] w-[30rem]'>
+        <div className='flex flex-col lg:flex-row gap-20'><div className=' h-[20rem] w-[20rem] md:w-[30rem] bg-slate-600'>.</div>
+      <div className=' bg-slate-500 h-[20rem] w-[20rem] md:w-[30rem]'>
       </div>
       </div>
-     <div className='flex gap-20'> <div className=' bg-slate-900 h-[20rem] w-[30rem]'></div>
+     <div className='hidden lg:flex gap-20'> <div className=' bg-slate-900 h-[20rem] w-[30rem]'></div>
       <div className='bg-slate-600 h-[20rem] w-[30rem]'></div></div>
       
       </div>
